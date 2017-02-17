9df949c2d549c8de5d3bef675529768847e34853
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2017-02-17 19:28:50 +0100

    Créer le serveur http pour le server 0

--- server/server.js
+++ server/server.js
@@ -3,46 +3,10 @@ const fs = require('fs')
 const app = express()
 const path = require('path')
 
-process.argv.forEach(function (val, index, array) {
-  console.log(index + ': ' + val);
+app.use(express.static('app/assets'));
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/app/index.html');
 });
-const http2 = process.argv[2] === 'http2';
 
-if (http2) {
-  const javascript = fs.readFileSync('./app/assets/script.js');
-  const stylesheet = fs.readFileSync('./app/assets/style.css');
-
-  app.get('/', function (req, res) {
-    res.push('/script.js', {
-      response: {
-        'content-type': 'application/javascript'
-      }
-    })
-      .end(javascript)
-
-    res.push('/style.css', {
-      response: {
-        'content-type': 'text/css'
-      }
-    })
-      .end(stylesheet)
-
-    res.sendFile(__dirname + '/app/index.html');
-  })
-
-  const options = {
-    key: fs.readFileSync('./server.key'),
-    cert: fs.readFileSync('./server.crt')
-  };
-
-  require('spdy').createServer(options, app).listen(3002);
-  console.log('Votre serveur HTTP tourne sur le port 3002');
-
-} else {
-  app.use(express.static('app/assets'));
-  app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/app/index.html');
-  })
-  app.listen(3003);
-  console.log('Votre serveur HTTP tourne sur le port 3003');
-}
+app.listen(3003);
+console.log('Votre serveur HTTP tourne sur le port 3003');
