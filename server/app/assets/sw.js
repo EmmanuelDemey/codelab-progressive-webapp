5e24310e6171db2b912aa11e2b34e1e1d2951e87
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2017-02-21 20:18:47 +0100

    Add SW for AppShell

---
+++ server/app/assets/sw.js
@@ -0,0 +1,35 @@
+const cacheName = 'codelab-1';
+
+const filesToCache = [
+  '/',
+  '/script.js',
+  '/style.css',
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
