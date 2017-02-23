a386e5fc774032538f55e40a6ed93ccb8aea62c3
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2017-02-23 21:48:27 +0100

    Add REST

--- server/server.js
+++ server/server.js
@@ -3,30 +3,39 @@ const fs = require('fs')
 const app = express()
 const path = require('path')
 
+const data = require('./data.json');
+
 const http2 = process.argv[2] === 'http2';
+
+app.get('/articles', function (req, res) {
+  res.send(data);
+});
+
+app.get('/like', function (req, res) {
+  data.highlight.ilike = !data.highlight.ilike;
+  res.status(200).end();
+});
 
 if (http2) {
   const javascript = fs.readFileSync('./app/assets/script.js');
-  const stylesheet = fs.readFileSync('./app/assets/style.css');
-
+  const stylesheet = fs.readFileSync('./app/assets/css/main.css');
+  const font = fs.readFileSync('./app/assets/css/exo-medium-webfont.woff');
+  const font2 = fs.readFileSync('./app/assets/css/exo-medium-webfont.woff2');
+  const logo = fs.readFileSync('./app/assets/imgs/logo.svg');
+  const like = fs.readFileSync('./app/assets/imgs/like.svg');
+  
   app.get('/', function (req, res) {
-    res.push('/script.js', {
-      response: {
-        'content-type': 'application/javascript'
-      }
-    })
-      .end(javascript)
-
-    res.push('/style.css', {
-      response: {
-        'content-type': 'text/css'
-      }
-    })
-      .end(stylesheet)
-
+    res.push('/script.js', { response: { 'content-type': 'application/javascript' } }).end(javascript)
+    res.push('/css/main.css', { response: { 'content-type': 'text/css' } }).end(stylesheet)
+    res.push('/css/exo-medium-webfont.woff', { response: { 'content-type': 'font/woff' } }).end(font)
+    res.push('/css/exo-medium-webfont.woff2', { response: { 'content-type': 'font/woff2' } }).end(font2)
+    res.push('/imgs/logo.svg', { response: { 'content-type': 'image/svg+xml' } }).end(logo)
+    res.push('/imgs/like.svg', { response: { 'content-type': 'image/svg+xml' } }).end(like)
     res.sendFile(__dirname + '/app/index.html');
   })
 
+  app.use(express.static('app/assets'));
+  
   const options = {
     key: fs.readFileSync('./server.key'),
     cert: fs.readFileSync('./server.crt')
@@ -40,6 +49,7 @@ if (http2) {
   app.get('/', function (req, res) {
     res.sendFile(__dirname + '/app/index.html');
   })
+  
   app.listen(3003);
   console.log('Votre serveur HTTP tourne sur le port 3003');
 }
