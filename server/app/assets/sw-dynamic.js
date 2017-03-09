c21727a3dfb112e54b3715a6ca8829eb1f7cb50c
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2017-03-09 13:25:31 +0100

    Finish Background Sync event

--- server/app/assets/sw-dynamic.js
+++ server/app/assets/sw-dynamic.js
@@ -30,10 +30,7 @@ self.addEventListener('fetch', function (e) {
 });
 
 function sendLike(){
-  //return fetch('/like');
-  return new Promise((resolve, reject) => {
-    resolve(true);
-  })
+  return fetch('/like');
 }
 
 self.addEventListener('sync', function(event) {
