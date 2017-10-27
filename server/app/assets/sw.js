8685f86dd9567c6ee8b04febe2f93bdc5f6352a8
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2017-10-27 14:05:30 +0200

    run Prettier

--- server/app/assets/sw.js
+++ server/app/assets/sw.js
@@ -1,11 +1,9 @@
-self.addEventListener('fetch', function(e) {
-   
-  if(e.request.url.endsWith('.jpg')){
-      e.respondWith(fetch('/imgs/cat.gif'));
+self.addEventListener("fetch", function(e) {
+  if (e.request.url.endsWith(".jpg")) {
+    e.respondWith(fetch("/imgs/cat.gif"));
   }
 
   /*e.respondWith( new Response('<h1> Bonjour Devoxx </h1>', {
         headers: {'Content-Type': 'text/html'}
   }));*/
 });
-
