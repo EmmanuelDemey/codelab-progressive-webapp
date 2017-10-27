1aa4b528dc6f4f895aceaf182ec7c515d46e88b8
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2017-10-27 14:17:45 +0200

    Run Prettier

--- server/app/assets/sw.js
+++ server/app/assets/sw.js
@@ -1,37 +1,39 @@
-const cacheName = 'codelab-7';
+const cacheName = "codelab-7";
 
 const filesToCache = [
-  '/',
-  '/script.js',
-  '/js/localforage.js',
-  '/css/main.css',
-  '/css/exo-medium-webfont.woff',
-  '/css/exo-medium-webfont.woff2',
-  '/imgs/logo.svg',
-  '/imgs/like.svg'
+  "/",
+  "/script.js",
+  "/js/localforage.js",
+  "/css/main.css",
+  "/css/exo-medium-webfont.woff",
+  "/css/exo-medium-webfont.woff2",
+  "/imgs/logo.svg",
+  "/imgs/like.svg"
 ];
 
-self.addEventListener('install', function (e) {
+self.addEventListener("install", function(e) {
   e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
+    caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
     })
   );
 });
 
-self.addEventListener('activate', function (e) {
+self.addEventListener("activate", function(e) {
   e.waitUntil(
-    caches.keys().then(function (keyList) {
-      return Promise.all(keyList.map(function (key) {
-        if (key !== cacheName) {
-          return caches.delete(key);
-        }
-      }));
+    caches.keys().then(function(keyList) {
+      return Promise.all(
+        keyList.map(function(key) {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
 
-self.addEventListener('fetch', function (e) {
+self.addEventListener("fetch", function(e) {
   /*if(e.request.url.endsWith('.jpg')){
       e.respondWith(fetch('/imgs/cat.gif'));
   }*/
@@ -40,41 +42,38 @@ self.addEventListener('fetch', function (e) {
         headers: {'Content-Type': 'text/html'}
   }));*/
 
-
-  if (e.request.url.endsWith('.jpg')) {
+  if (e.request.url.endsWith(".jpg")) {
     e.respondWith(
       fetch(e.request)
-        .then(function (response) {
+        .then(function(response) {
           var copy = response.clone();
-          caches.open(cacheName)
-            .then(function (cache) {
-              cache.put(e.request, copy);
-            });
+          caches.open(cacheName).then(function(cache) {
+            cache.put(e.request, copy);
+          });
           return response;
         })
-        .catch(function () {
-          return caches.match(e.request)
-            .then(function (response) {
-              return response;
-            })
+        .catch(function() {
+          return caches.match(e.request).then(function(response) {
+            return response;
+          });
         })
     );
   } else {
     e.respondWith(
-      caches.match(e.request).then(function (response) {
+      caches.match(e.request).then(function(response) {
         return response || fetch(e.request);
       })
     );
   }
 });
 
-function sendLike(){
-  return fetch('/like');
+function sendLike() {
+  return fetch("/like");
 }
 
-self.addEventListener('sync', function(event) {
-  console.log(event)
-  if (event.tag == 'like') {
+self.addEventListener("sync", function(event) {
+  console.log(event);
+  if (event.tag == "like") {
     event.waitUntil(sendLike());
   }
 });
