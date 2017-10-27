e35aa5f1b35f68e85e46d6c31d875aa6fb3359c3
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2017-10-27 14:14:59 +0200

    Run Prettier

--- server/app/assets/script.js
+++ server/app/assets/script.js
@@ -1,70 +1,77 @@
 window.onload = function() {
-    toLike();
+  toLike();
 
-    let promise;
-        
-    if(!navigator.onLine) {
-        document.querySelector('#wrapper').classList.add('off');
-        promise = localforage.getItem('data');
-    } else {
-        promise = fetch('/articles')
-                .then(result => result.json());
-    }
+  let promise;
 
-    
-    promise.then(data => {
-        displayData(data);
-        return data;
+  if (!navigator.onLine) {
+    document.querySelector("#wrapper").classList.add("off");
+    promise = localforage.getItem("data");
+  } else {
+    promise = fetch("/articles").then(result => result.json());
+  }
+
+  promise
+    .then(data => {
+      displayData(data);
+      return data;
     })
-    .then(data => localforage.setItem('data', data))
+    .then(data => localforage.setItem("data", data));
 
-    window.addEventListener("offline", function () {
-        document.querySelector('#wrapper').classList.add('off');
-    }, false);
-    window.addEventListener("online", function () {
-        document.querySelector('#wrapper').classList.remove('off');
-    }, false);
+  window.addEventListener(
+    "offline",
+    function() {
+      document.querySelector("#wrapper").classList.add("off");
+    },
+    false
+  );
+  window.addEventListener(
+    "online",
+    function() {
+      document.querySelector("#wrapper").classList.remove("off");
+    },
+    false
+  );
 
-    if ('serviceWorker' in navigator) {
-		navigator.serviceWorker
-				.register('./sw.js')
-				.then(function() { console.log('Service Worker Registered'); });
-	}
-    
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js").then(function() {
+      console.log("Service Worker Registered");
+    });
+  }
 };
 
 function displayData(data) {
-    let highlight = document.querySelector('.highlight');
-    highlight.querySelector('h2 a').innerHTML = data.highlight.title;
-    highlight.querySelector('img').src = data.highlight.picture;
-    highlight.querySelector('.description').innerHTML = data.highlight.description;
-    highlight.querySelector('.info li:first-child').innerHTML = data.highlight.date;
-    highlight.querySelector('.info li:nth-child(2)').innerHTML = `${data.highlight.like} j'aime`;
-    highlight.classList.remove('hidden');
+  let highlight = document.querySelector(".highlight");
+  highlight.querySelector("h2 a").innerHTML = data.highlight.title;
+  highlight.querySelector("img").src = data.highlight.picture;
+  highlight.querySelector(".description").innerHTML =
+    data.highlight.description;
+  highlight.querySelector(".info li:first-child").innerHTML =
+    data.highlight.date;
+  highlight.querySelector(".info li:nth-child(2)").innerHTML = `${data.highlight
+    .like} j'aime`;
+  highlight.classList.remove("hidden");
 
-    if(data.highlight.ilike){
-        highlight.querySelector('.icon-like').classList.add('active');  
-    }
+  if (data.highlight.ilike) {
+    highlight.querySelector(".icon-like").classList.add("active");
+  }
 
-    data.entries.forEach((entry, i) => {
-        let e = document.querySelector(`.entry:nth-child(${i + 1})`)
-        e.querySelector('h2').innerHTML = entry.title;
-        e.querySelector('img').src = entry.picture;
-        e.querySelector('.content p').innerHTML = entry.content;
-        e.classList.remove('hidden');
-    });
-
+  data.entries.forEach((entry, i) => {
+    let e = document.querySelector(`.entry:nth-child(${i + 1})`);
+    e.querySelector("h2").innerHTML = entry.title;
+    e.querySelector("img").src = entry.picture;
+    e.querySelector(".content p").innerHTML = entry.content;
+    e.classList.remove("hidden");
+  });
 }
 
 function toLike() {
-    const zLike = document.querySelectorAll('.icon-like');
+  const zLike = document.querySelectorAll(".icon-like");
 
-    zLike.forEach(el => {
-        el.addEventListener('click', function() {
-            fetch('/like').then(() => {
-                    this.classList.toggle('active');
-                });
-        });
+  zLike.forEach(el => {
+    el.addEventListener("click", function() {
+      fetch("/like").then(() => {
+        this.classList.toggle("active");
+      });
     });
+  });
 }
-
--- server/server.js
+++ server/server.js
@@ -1,58 +1,77 @@
-const express = require('express')
-const fs = require('fs')
-const app = express()
-const path = require('path')
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const path = require("path");
 
-const data = require('./data.json');
+const data = require("./data.json");
 
-const http2 = process.argv[2] === 'http2';
+const http2 = process.argv[2] === "http2";
 
-app.get('/articles', function (req, res) {
+app.get("/articles", function(req, res) {
   res.send(data);
 });
 
-app.get('/like', function (req, res) {
+app.get("/like", function(req, res) {
   data.highlight.ilike = !data.highlight.ilike;
   res.status(200).end();
 });
 
 if (http2) {
-  const javascript = fs.readFileSync('./app/assets/script.js');
-  const stylesheet = fs.readFileSync('./app/assets/css/main.css');
-  const font = fs.readFileSync('./app/assets/css/exo-medium-webfont.woff');
-  const font2 = fs.readFileSync('./app/assets/css/exo-medium-webfont.woff2');
-  const logo = fs.readFileSync('./app/assets/imgs/logo.svg');
-  const like = fs.readFileSync('./app/assets/imgs/like.svg');
-  
-  app.get('/', function (req, res) {
-    res.push('/script.js', { response: { 'content-type': 'application/javascript' } }).end(javascript)
-    res.push('/css/main.css', { response: { 'content-type': 'text/css' } }).end(stylesheet)
-    res.push('/css/exo-medium-webfont.woff', { response: { 'content-type': 'font/woff' } }).end(font)
-    res.push('/css/exo-medium-webfont.woff2', { response: { 'content-type': 'font/woff2' } }).end(font2)
-    res.push('/imgs/logo.svg', { response: { 'content-type': 'image/svg+xml' } }).end(logo)
-    res.push('/imgs/like.svg', { response: { 'content-type': 'image/svg+xml' } }).end(like)
-    res.sendFile(__dirname + '/app/index.html');
-  })
+  const javascript = fs.readFileSync("./app/assets/script.js");
+  const stylesheet = fs.readFileSync("./app/assets/css/main.css");
+  const font = fs.readFileSync("./app/assets/css/exo-medium-webfont.woff");
+  const font2 = fs.readFileSync("./app/assets/css/exo-medium-webfont.woff2");
+  const logo = fs.readFileSync("./app/assets/imgs/logo.svg");
+  const like = fs.readFileSync("./app/assets/imgs/like.svg");
 
-  app.use(express.static('app/assets'));
-  app.use(express.static('node_modules/sw-toolbox'));
-  
+  app.get("/", function(req, res) {
+    res
+      .push("/script.js", {
+        response: { "content-type": "application/javascript" }
+      })
+      .end(javascript);
+    res
+      .push("/css/main.css", { response: { "content-type": "text/css" } })
+      .end(stylesheet);
+    res
+      .push("/css/exo-medium-webfont.woff", {
+        response: { "content-type": "font/woff" }
+      })
+      .end(font);
+    res
+      .push("/css/exo-medium-webfont.woff2", {
+        response: { "content-type": "font/woff2" }
+      })
+      .end(font2);
+    res
+      .push("/imgs/logo.svg", { response: { "content-type": "image/svg+xml" } })
+      .end(logo);
+    res
+      .push("/imgs/like.svg", { response: { "content-type": "image/svg+xml" } })
+      .end(like);
+    res.sendFile(__dirname + "/app/index.html");
+  });
+
+  app.use(express.static("app/assets"));
+  app.use(express.static("node_modules/sw-toolbox"));
+
   const options = {
-    key: fs.readFileSync('./server.key'),
-    cert: fs.readFileSync('./server.crt')
+    key: fs.readFileSync("./server.key"),
+    cert: fs.readFileSync("./server.crt")
   };
 
-  require('spdy').createServer(options, app).listen(3002);
-  console.log('Votre serveur HTTP tourne sur le port 3002');
-
+  require("spdy")
+    .createServer(options, app)
+    .listen(3002);
+  console.log("Votre serveur HTTP tourne sur le port 3002");
 } else {
-  app.use(express.static('app/assets'));
-  app.use(express.static('node_modules/sw-toolbox'));
+  app.use(express.static("app/assets"));
+  app.use(express.static("node_modules/sw-toolbox"));
 
-  app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/app/index.html');
-  })
-  
+  app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/app/index.html");
+  });
+
   app.listen(3003);
-  console.log('Votre serveur HTTP tourne sur le port 3003');
+  console.log("Votre serveur HTTP tourne sur le port 3003");
 }
